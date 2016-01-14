7798bc37d9ed63ce3bf1eb04e01f7ab74e884369
Alexsjones <tibbar@freedommail.ch>
2016-01-14 12:39:44 +0000

    setting up a basic project template for unix C projects

---
+++ src/example.c
@@ -0,0 +1,9 @@
+/*********************************************************************************
+*     File Name           :     /home/anon/Code/c_template/src/example.c
+*     Created By          :     anon
+*     Creation Date       :     [2016-01-14 12:38]
+*     Last Modified       :     [AUTO_UPDATE_BEFORE_SAVE]
+*     Description         :      
+**********************************************************************************/
+#include "example.h"
+
